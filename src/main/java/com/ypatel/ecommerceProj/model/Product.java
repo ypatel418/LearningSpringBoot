7eda3a71ea92d82073619ed581b68fd24f1b7dcc
ypatel418 <ypatel41805@gmail.com>
2026-01-07 23:09:01 +0530

    added a bunch of new features and finished the project

--- src/main/java/com/ypatel/ecommerceProj/model/Product.java
+++ src/main/java/com/ypatel/ecommerceProj/model/Product.java
@@ -3,10 +3,13 @@ package com.ypatel.ecommerceProj.model;
 import java.math.BigDecimal;
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,12 +24,18 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
-    private String desc;
+    private String description;
     private String brand;
     private BigDecimal price;
     private String category;
-    private Date releaseDate;
-    private boolean available;
-    private int quantity;
 
+    // @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM-dd-yyyy")
+    private Date releaseDate;
+    private boolean productAvailable;
+    private int stockQuantity;
+
+    private String imageName;
+    private String imageType;
+    @Lob
+    private byte[] imageDate;
 }
--- src/main/java/com/ypatel/ecommerceProj/service/ProductService.java
+++ src/main/java/com/ypatel/ecommerceProj/service/ProductService.java
@@ -1,9 +1,11 @@
 package com.ypatel.ecommerceProj.service;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.ypatel.ecommerceProj.model.Product;
 import com.ypatel.ecommerceProj.repository.ProductRepo;
@@ -16,6 +18,32 @@ public class ProductService {
 
     public List<Product> getAllProducts() {
         return repo.findAll();
+    }
+
+    public Product getProductById(int id) {
+        return repo.findById(id).orElse(null);
+    }
+
+    public Product addProduct(Product product, MultipartFile imageFile) throws IOException {
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        product.setImageDate(imageFile.getBytes());
+        return repo.save(product);
+    }
+
+    public Product updateProduct(int id, Product product, MultipartFile imageFile) throws IOException {
+        product.setImageDate(imageFile.getBytes());
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        return repo.save(product);
+    }
+
+    public void deleteProduct(int id) {
+        repo.deleteById(id);
+    }
+
+    public List<Product> searchProducts(String keyword) {
+        return repo.searchProducts(keyword);
     }
 
 }
